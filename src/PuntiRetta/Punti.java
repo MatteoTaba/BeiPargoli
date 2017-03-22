c4edc4bff9b20c65a4812bdda61ad7fef8c26bd8
Matteo Tabarelli <matteo.tabarelli1997@gmail.com>
2017-03-22 16:11:42 +0100

    Ottimizzazione equals

--- src/PuntiRetta/Punti.java
+++ src/PuntiRetta/Punti.java
@@ -26,19 +26,18 @@ public class Punti
 		return  Math.sqrt (Math.pow ( (x-punto2.getX() ), 2) + Math.pow( (y-punto2.getY() ), 2) );
 	}
 	
-	public boolean equals(Punti p1, Punti p2)
+	public boolean equals(Punti p2) //verifica dell'uguaglianza dei punti
 	{
 		boolean stato = false;
-		if(p1.getX()==p2.getX() && p1.getY()==p2.getY())
+		if(this.x==p2.x && this.y==p2.y)
 		{
 			stato = true;
 		}		
 		return stato;
 	}
 	
-	public String toString(){
+	public String toString() //traduzione delle coordinate del punto in stringa
+	{
 		return "("+this.x+","+this.y+")";
-	}
-		
-	
+	}	
 }
