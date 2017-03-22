ad416a6dd541c06771d3a7dd821dc985cd2c2fcf
Matteo Tabarelli <matteo.tabarelli1997@gmail.com>
2017-03-22 16:13:12 +0100

    Piccola modificato ultimo system out

--- src/PuntiRetta/Main.java
+++ src/PuntiRetta/Main.java
@@ -4,27 +4,24 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		Punti punto1=new Punti(1,2);
-		Punti punto2=new Punti(2,3);
+		Punti punto1=new Punti(4,6);
+		Punti punto2=new Punti(5,2);
 		Punti punto3=new Punti(0,2);
-		
 		double distanza;
 		
 		Rette retta=new Rette(punto1, punto2);
 		
-		
-		
 		distanza=punto1.distance(punto2);
 		System.out.println("Distanza tra "+punto1.toString()+" e "+punto2.toString()+" = "+distanza);
 		
-		String equazione=retta.equazione();
+		String equazione=retta.equazione(punto1, punto2);
 		System.out.println("Equazione retta : "+equazione);
 		
 		boolean appartiene=retta.appartenenza(punto3);
 		if(appartiene)
-			System.out.println("Punto "+punto3.toString()+" appartiene alla retta : "+equazione);
+			System.out.println("Punto "+punto3.toString()+" appartiene alla retta");
 		else
-			System.out.println("Punto "+punto3.toString()+" non appartiene alla retta : "+equazione);
+			System.out.println("Punto "+punto3.toString()+" non appartiene alla retta");
 
 	}
 }
