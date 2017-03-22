768c6039d27016b4c1876fa9e0136d1989345e14
Matteo Tabarelli <matteo.tabarelli1997@gmail.com>
2017-03-22 16:09:00 +0100

    Rename Main.java to Rette.java

--- src/PuntiRetta/Rette.java
+++ src/PuntiRetta/Rette.java
@@ -5,41 +5,53 @@ public class Rette
 	private double m;
 	private double q;
 	private boolean verticale;
+	private boolean uguali;
 	
-	public Rette(Punti p1, Punti p2)
+	public Rette(Punti p1, Punti p2) //costruttore con compito di calcolare m e q, verificare la diversità e la verticalità dei punti
 	{
-		if(p1.getX()==p2.getX())
+		if(p1.equals(p2)==false)
 		{
-			q=p1.getX();
-			verticale=true;
+			if(p1.getX()==p2.getX())
+			{
+				q=p1.getX();
+				verticale=true;
+			}
+			else
+			{
+				verticale=false;
+				m=(p2.getY() - p1.getY())/(p2.getX() - p1.getX());
+				q= p2.getY()-(m*p2.getX());
+			}
 		}
 		else
 		{
-			verticale=false;
-			m=(p2.getY() - p1.getY())/(p2.getX() - p1.getX());
-			q= p2.getY()-(m*p2.getX());
+			uguali=true;
 		}
 	}
 	
-	public String equazione()
+	public String equazione(Punti p1, Punti p2) //elaborazione dell'equazione
 	{
 		if(verticale==true)
 		{
 			return "x = " + this.q;
 		}
-		else
+		else if(verticale==false && uguali==false)
 		{
 			if(this.m==0.0)
 				return "y = " + this.q;
 			else
 				return "y = " + this.m + "x + " + this.q;
 		}
+		else 
+		{
+			return "Non posso calcolare l'equazione";
+		}
 		
 	}
 	
 	//TODO metodo equazioneFormatted con DecimalFormatter come parametro
 	
-	public boolean appartenenza(Punti p3)
+	public boolean appartenenza(Punti p3) //verifica se il terzo punto appartiene alla retta
 	{
 		if(verticale==true)
 		{
