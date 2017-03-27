518e111d88c23c423b194fdb7490c03a5b3e9b8f
Riccardo <Riccardo@10.0.81.155>
2017-03-27 17:11:28 +0200

    Aggiungo il mio.

---
+++ src/TamagotchiPerno/Main.java
@@ -0,0 +1,23 @@
+package TamagotchiPerno;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	saluta();
+	do{
+		System.out.println("1-Accarezza");
+		System.out.println("2-Dai biscotto");
+		System.out.println("3-Esci");
+		Scanner read = new Scanner(System.in);
+		
+	}while(comando!=3);
+
+	}
+	
+	public static void saluta(){
+		System.out.println("Ciao");
+	}
+
+}
