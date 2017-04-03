8e27d714e1eb1653501dec4ad4a4b4bfb2939e2c
lanzi97 <lanzi97@hotmail.it>
2017-04-03 17:13:19 +0200

    MyMenu.. marcio

--- src/archivioCD/CDmain.java
+++ src/archivioCD/CDmain.java
@@ -1,17 +1,15 @@
 package archivioCD;
 
 public class CDmain {
-  
-  
-  public static void main(String args[]){
-    System.out.println("Mona");
-    MyMenu menu1=createMenu();
-    menu1.selectChoice();
-  }
-  
-  private static MyMenu createMenu()
-  {
-    String ma[]={"Esci", "bo", "acas", "asdasd"};
-    return new MyMenu("Titolo", ma);
-  }
+	public static void main(String args[]){
+		
+		MyMenu menu1=createMenu();
+		menu1.selectChoice();
+	}
+	
+	private static MyMenu createMenu()
+	{
+		String ma[]={"Esci", "bo", "acas", "asdasd"};
+		return new MyMenu("Titolo", ma);
+	}
 }
--- src/archivioCD/MyMenu.java
+++ src/archivioCD/MyMenu.java
@@ -4,29 +4,31 @@ import java.util.*;
 import gruppoA.util.*;
 
 public class MyMenu {
-  private String title;
-  private String choises[];
+	private String title;
+	private String choises[];
   
-  private static final String MSG_INS_CHOICE="Scelta : ";
+	private static final String MSG_INS_CHOICE="Scelta : ";
   
-  public MyMenu(String title, String[] choises){
-    this.title=title;
-    this.choises=choises;
-  }
+	public MyMenu(String title, String[] choises){
+		this.title=title;
+		this.choises=choises;
+	}
   
-  public void showMenu(){
-    System.out.println(title);
-    int i=0;
-    for(String choice : choises)
-    {
-      System.out.println(i+choice);
-      i++;
-    }
-  }
-  
-  public int selectChoice()
-  {
-    showMenu();
-    return KeyboardInput.acquireInt(0, choises.size()-1, MSG_INS_CHOICE);
-  }
+	public void showMenu(){
+		System.out.println(title);
+		int i=0;
+		for(String choice : choises)
+		{
+			System.out.println(i+"\t"+choice);
+			i++;
+		}
+	}
+	
+	public int selectChoice()
+	{
+		showMenu();
+		int from=0;
+		int to=choises.length-1;
+		return KeyboardInput.acquireInt(from, to, MSG_INS_CHOICE);
+	}
 }
