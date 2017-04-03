3961795c2627f7df851d5c1b144f3c8eaa26cb64
lanzi97 <lanzi97@hotmail.it>
2017-04-03 16:57:35 +0200

    BO

--- src/archivioCD/CDmain.java
+++ src/archivioCD/CDmain.java
@@ -1,8 +1,17 @@
 package archivioCD;
 
 public class CDmain {
-
+  
+  
   public static void main(String args[]){
-    System.out.println("Prova");
+    System.out.println("Mona");
+    MyMenu menu1=createMenu();
+    menu1.selectChoice();
+  }
+  
+  private static MyMenu createMenu()
+  {
+    String ma[]={"Esci", "bo", "acas", "asdasd"};
+    return new MyMenu("Titolo", ma);
   }
 }
