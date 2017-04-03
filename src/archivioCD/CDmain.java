2921f7195cb8484214e04c1afc0bd3f730bb8d51
lanzi97 <lanzi97@hotmail.it>
2017-04-03 21:34:22 +0200

    Menu choises sentences

--- src/archivioCD/CDmain.java
+++ src/archivioCD/CDmain.java
@@ -1,15 +1,19 @@
 package archivioCD;
 
 public class CDmain {
+	
+	private static final String MENU_PRIMARY_CHOISE[]={"Esci", "Gestisci archivio", "Gestisci CD"};
+	private static final String MENU_ARCHIVE_CHOISE[]={"Indietro", "Visualizza tutti i CD", "Trova CD", "Ottieni CD random", "Inserisci nuovo CD", "Elimina CD"};
+	private static final String MENU_CD_CHOISE[]={"Indietro", "Visualizza tracce", "Ottieni traccia", "Ottieni traccia random", "Inserisci nuova traccia", "Elimina traccia"};
+	
 	public static void main(String args[]){
 		
-		MyMenu menu1=createMenu();
-		menu1.selectChoice();
+		MyMenu menuPrimary=createMenu(MENU_PRIMARY_CHOISE);
+		menuPrimary.selectChoice();
 	}
 	
-	private static MyMenu createMenu()
+	private static MyMenu createMenu(String choises[])
 	{
-		String ma[]={"Esci", "bo", "acas", "asdasd"};
-		return new MyMenu("Titolo", ma);
+		return new MyMenu("Titolo", choises);
 	}
 }
