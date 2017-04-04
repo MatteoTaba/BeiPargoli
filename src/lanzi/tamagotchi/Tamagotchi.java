bf195f2d7a641994e6230867e58fafe6836004e1
lanzi97 <lanzi97@hotmail.it>
2017-04-04 17:30:56 +0200

    Adjustment for Javadoc

--- src/lanzi/tamagotchi/Tamagotchi.java
+++ src/lanzi/tamagotchi/Tamagotchi.java
@@ -1,5 +1,8 @@
 package lanzi.tamagotchi;
 
+/**
+ * Classe per la creazione e gestione del Tamagotchi.
+ */
 public class Tamagotchi{
 	private int satisfactionGrade;
 	private int fullGrade;
@@ -10,11 +13,9 @@ public class Tamagotchi{
 	
 	private static final float FULLPERBISCUITPERCENT=10;
 	
-	/*
-	 * Costruttore di default.
-	 * Assegna -nome:Tamagotchi
-	 * 		   -grado di soddisfazione:50
-	 * 		   -grado di sazieta':50
+	/**
+	 * Costruttore di default.<br>
+	 * Assegna nome:Tamagotchi, grado di soddisfazione:50, grado di sazieta':50.
 	 */
 	public Tamagotchi(){
 		name="Tamagotchi";
@@ -22,10 +23,9 @@ public class Tamagotchi{
 		fullGrade=50;
 	}
 	
-	/*
-	 * Costruttore con parametri -grado di soddisfazione
-	 * 							 -grado di sazietà
-	 * Assegna -nome:Tamagotchi
+	/**
+	 * Costruttore con parametri grado di soddisfazione, grado di sazietà.<br>
+	 * Assegna nome:Tamagotchi.
 	 */
 	public Tamagotchi(int fullGrade,int satisfactionGrade){
 		name="Tamagotchi";
@@ -33,10 +33,8 @@ public class Tamagotchi{
 		this.satisfactionGrade=satisfactionGrade;
 	}
 	
-	/*
-	 * Costruttore con parametri -nome
-	 * 							 -grado di soddisfazione
-	 * 							 -grado di sazietà
+	/**
+	 * Costruttore con parametri nome, grado di soddisfazione, grado di sazietà.
 	 */
 	public Tamagotchi(String name, int fullGrade,int satisfactionGrade){
 		this.name=name;
@@ -44,50 +42,50 @@ public class Tamagotchi{
 		this.satisfactionGrade=satisfactionGrade;
 	}
 	
-	/*
+	/**
 	 * Getter grado di soddisfazione
-	 * @return  grado di soddisfazione
+	 * @return  Grado di soddisfazione
 	 */
 	public int getSatisfactionGrade(){
 		return satisfactionGrade;
 	}
 	
-	/*
+	/**
 	 * Getter grado di sazietà
-	 * @return  grado di sazietà
+	 * @return  Grado di sazietà
 	 */
 	public int getFullGrade(){
 		return fullGrade;
 	}
 	
-	/*
+	/**
 	 * Getter nome
-	 * @return  nome
+	 * @return  Nome
 	 */
 	public String getName(){
 		return name;
 	}
 	
-	/*
+	/**
 	 * Setter grado di soddisfazione
-	 * @param int  grado di soddisfazione
+	 * @param int  Grado di soddisfazione
 	 */
 	public void setSatisfactionGrade(int satisfactionGrade){
 		this.satisfactionGrade=satisfactionGrade;
 	}
 	
-	/*
+	/**
 	 * Getter grado di sazietà
-	 * @param int  grado di sazietà
+	 * @param int  Grado di sazietà
 	 */
 	public void setFullGrade(int fullGrade){
 		this.fullGrade=fullGrade;
 	}
 	
-	/*
+	/**
 	 * Aumenta il grado di sazietà in percentuale rispetto all'attuale livello.
 	 * Diminuisce il grado di soddisfazione di quantità/4 punti.
-	 * @param int  quantità dei biscotti
+	 * @param int  Quantità dei biscotti
 	 */
 	public void eatBiscuit(int quantity){
 		for(int i=0;i<quantity;i++){
@@ -96,10 +94,10 @@ public class Tamagotchi{
 		satisfactionGrade=Math.round(satisfactionGrade-(float)quantity/4);
 	}
 	
-	/*
+	/**
 	 * Aumenta il grado di soddisfazione di una certa quantità.
 	 * Diminuisce il grado di sazietà della metà dell'aumento del grado di soddisfazione.
-	 * @param int  quantità delle carezze
+	 * @param int  Quantità delle carezze
 	 */
 	public void receiveCaress(int quantity){
 		satisfactionGrade=Math.round(satisfactionGrade+quantity*SATISFACTIONPERCARESS);
@@ -108,17 +106,17 @@ public class Tamagotchi{
 			satisfactionGrade=100;
 	}
 	
-	/*
+	/**
 	 * Verifica lo stato di vita
-	 * @return boolean  morto=true / vivo=false
+	 * @return boolean  Morto=true / Vivo=false
 	 */
 	public boolean dead(){
 		return (satisfactionGrade<=0 || fullGrade<=0 || fullGrade>100);
 	}
 	
-	/*
+	/**
 	 * Verifica lo stato di felicità
-	 * @return boolean  infelice=true / felice=false
+	 * @return boolean  Infelice=true / Felice=false
 	 */
 	public boolean unhappy(){
 		return (fullGrade<30 || fullGrade>90);
